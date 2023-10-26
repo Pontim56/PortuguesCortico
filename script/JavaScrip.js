87e20bdad389a99594eb35b781fe47e9f8fb7cfc
Pontim56 <leonardopontimp@hotmail.com>
2023-10-26 19:20:31 -0300

    teste

--- script/JavaScrip.js
+++ script/JavaScrip.js
@@ -1,28 +1,15 @@
-// var flipTimeout;
+var flipTimeout;
 
-// function flipCard(card) {
-//   clearTimeout(flipTimeout); 
-//   card.querySelector('.flip-card-inner').style.transform = 'rotateY(-180deg)';
-// }
+function flipCard(card) {
+  clearTimeout(flipTimeout); 
+  card.querySelector('.flip-card-inner').style.transform = 'rotateY(-180deg)';
+}
 
-// function unflipCard(card) {
-//   clearTimeout(flipTimeout); 
-//   flipTimeout = setTimeout(function() {
-//     card.querySelector('.flip-card-inner').style.transform = 'rotateY(0deg)';
-//   }, 220);
-// }
+function unflipCard(card) {
+  clearTimeout(flipTimeout); 
+  flipTimeout = setTimeout(function() {
+    card.querySelector('.flip-card-inner').style.transform = 'rotateY(0deg)';
+  }, 220);
+}
 
-
-const myObserver = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=> {
-    if (entry.isIntersecting){
-      entry.target.classList.add('show')
-    }else{
-      entry.target.classList.remove('show')
-    }
-  })
-})
-const elements=document.querySelectorAll('.hidden')
-
-elements.forEach((element)=>myObserver.observe(element))
 

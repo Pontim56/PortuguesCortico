ddd5854baee8efb078cdcd2e525dd7431e6fdd82
Pontim56 <leonardopontimp@hotmail.com>
2023-11-07 21:52:46 -0300

    tste

--- script/jsAnmt.js
+++ script/jsAnmt.js
@@ -16,6 +16,25 @@ elements.forEach((element)=>myObserver.observe(element))
 
 
 
-function menufc(){
-    var 
+// function mostrarMenu(){
+//     var menu = document.getElementById("menu");
+//     if (menu.style.display === "none") {
+//         menu.style.display === "block";
+//         document.getElementById("menu").innerText="oi";
+
+//     } else {
+//         menu.style.display ==="none";
+//     }
+// }
+function mostrarMenu(event){
+    var menu = document.getElementById("menu");
+    var estilo = window.getComputedStyle(menu);
+    var nav = document.getElementById("nav");
+    if (estilo.display==="none") {
+        menu.style.display="block";
+        nav.style.backgroundColor="rgb(134, 197, 218)";
+    }else{
+        menu.style.display="none";
+        nav.style.backgroundColor="rgb(207, 229, 236)";
+    }
 }

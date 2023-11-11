86f1629f60cc587acfb5e3483678add2f90856f3
Pontim56 <leonardopontimp@hotmail.com>
2023-11-10 21:50:04 -0300

    teste

--- script/JavaScrip.js
+++ script/JavaScrip.js
@@ -11,3 +11,32 @@ function unflipCard(card) {
     card.querySelector('.flip-card-inner').style.transform = 'rotateY(0deg)';
   }, 220);
 }
+
+
+
+function rafa(){
+  var sandra=document.getElementById("sandra");
+  var estilo = window.getComputedStyle(sandra);
+  var liberatoCTT = document.getElementById("liberatoContent");
+  if (estilo.display==="flex") {
+    sandra.style.display="none";
+    liberatoCTT.style.display="block";
+
+}else{
+  sandra.style.display="flex";
+  liberatoCTT.style.display="none"
+}
+}
+function sandra(){
+  var rafa=document.getElementById("liberato");
+  var estilo = window.getComputedStyle(liberato);
+  var sandraCTT = document.getElementById("sandraContent");
+  if (estilo.display==="flex") {
+    rafa.style.display="none";
+    sandraCTT.style.display="block";
+
+}else{
+  rafa.style.display="flex";
+  sandraCTT.style.display="none"
+}
+}
